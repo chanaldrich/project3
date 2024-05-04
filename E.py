0619c51aecfdcb72a2f25c678a56cf6728ca81de
Aldrich <aldrichpu@gmail.com>
2024-05-04 11:03:05 +0800

    git and py code

---
+++ E.py
@@ -0,0 +1,38 @@
+# len,find,capitalize,upper,lower
+# name ="pro gamer B)"
+# length =len(name)
+# print("your full name have",length,"in it.")
+
+# space_pos=name.find(" ")
+# print("your first space is on",space_pos)
+
+# name_capitalized=name.capitalize()
+# print("your full name(first letter capital)",name_capitalized)
+
+# name_upper=name.upper()
+# print("your full name(all letter capital)",name_upper)
+
+# name_lower=name.lower()
+# print("your full name(all letter capital)",name_lower)
+#-------------------------------------------------------------------------------
+# count,replace
+
+# phone_number = input("put in your phone number:")
+# dash_count =phone_number.count("-")
+# print("your phone number has",dash_count,"dash in it")
+
+# phone_number = phone_number.replace("-"," ")
+# print("your phone number",phone_number)
+#-------------------------------------------------------------------------------
+# username with only english letters and less then 12 letters and no spaces
+# username=input("please put in your new username:")
+
+# if len(username) >12:
+#     print("you can't have more then 12 letters")
+# elif " " in username:
+#     print("you can't have any spaces in your username")
+# elif not username.isalpha():
+#     print("your username can only have english letters in it")
+# else:
+#     print('welcome '+username)
+#-------------------------------------------------------------------------------
