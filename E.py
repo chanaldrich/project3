5e9497a0574192315730437fc08d29d7bd47adfc
Aldrich <aldrichpu@gmail.com>
2024-07-22 09:26:13 +0800

    python

--- E.py
+++ E.py
@@ -750,25 +750,30 @@
 # cube = Cube(10, 20, 30)
 # print(cube)
 # -------------------------------------------------------------------------------
+# object as argument
+
+# class Car:
+#     color = None
+# def change_color(car, color):
+#     car.color = color
+
+# car1=Car()
+# car2=Car()
+# car3=Car()
+
+# print(car1.color)
+# print(car2.color)
+# print(car3.color)
+
+# change_color(car1, "red")
+# change_color(car2, "blue")
+# change_color(car3, "black")
+
+# print(car1.color)
+# print(car2.color)
+# print(car3.color)
+# -------------------------------------------------------------------------------
 # 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
