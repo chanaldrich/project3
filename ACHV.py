f293f6d4660c7973b3884366d1d93ab99604ec15
Aldrich <aldrichpu@gmail.com>
2024-08-23 09:20:02 +0800

    python

--- ACHV.py
+++ ACHV.py
@@ -1,5 +1,385 @@
-# 【ACHV】：multiplication table
-for i in range(1, 10):
-    for j in range(1, 10):
-        print(f"{i} * {j} = {i * j}", end="    ")
-    print()
+# 【ACHV】: multiplication table
+
+# for i in range(1, 10):
+#     for j in range(1, 10):
+#         print(f"{i} * {j} = {i * j:2}", end="    ")
+#     print()
+# -----------------------------------------------------------------
+# 【ACHV】: car 
+# import turtle
+
+# def draw_rectangle(color, x, y, width, height):
+#     turtle.penup()
+#     turtle.goto(x,y)
+#     turtle.pendown()
+#     turtle.color(color)
+#     turtle.begin_fill()
+#     for _ in range(2):
+#         turtle.forward(width)
+#         turtle.right(90)
+#         turtle.forward(height)
+#         turtle.right(90)
+#     turtle.end_fill()
+
+# def draw_circle(color, x, y, radius):
+#     turtle.penup()
+#     turtle.goto(x, y)
+#     turtle.pendown()
+#     turtle.color(color)
+#     turtle.begin_fill()
+#     turtle.circle(radius)
+#     turtle.end_fill()
+
+# def draw_car():
+#     draw_rectangle("blue", -70, 0, 140, 50)
+#     draw_rectangle("lightblue", -50, 25, 100, 30)
+#     draw_circle("black", -35, -70, 15)
+#     draw_circle("black", 35, -70 ,15)
+
+# turtle.speed(1)
+# turtle.bgcolor("white")
+
+# draw_car()
+# turtle.hideturtle()
+# turtle.done()
+# -----------------------------------------------------------------
+# 【ACHV】: pig
+
+# import turtle
+# def draw_circle(color, x, y, radius):
+#     turtle.penup()
+#     turtle.goto(x, y)
+#     turtle.pendown()
+#     turtle.color(color)
+#     turtle.begin_fill()
+#     turtle.circle(radius)
+#     turtle.end_fill()
+
+# def draw_ear(x, y):
+#     turtle.penup()
+#     turtle.goto(x, y)
+#     turtle.pendown()
+#     turtle.color("pink")
+#     turtle.begin_fill()
+#     turtle.circle(30, steps=3)
+#     turtle.end_fill()
+
+# def draw_pig():
+#     turtle.speed(3)
+
+#     draw_circle("pink", 100, 0, -100)
+#     draw_circle("pink", 80, 0, 50)
+
+#     draw_ear(-60, 180)
+#     draw_ear(60, 180)
+
+#     draw_circle("white", 20, -30, 150)
+#     draw_circle("white", 20, 30, 150)
+#     draw_circle("black", 10, -30, 160)
+#     draw_circle("black", 10, 30, 160)
+
+
+#     draw_circle("pink", 40, 0, 100)
+#     draw_circle("black", 10, -15, 115)
+#     draw_circle("black", 10, 15, 115)
+
+#     turtle.penup()
+#     turtle.goto(-50,-200)
+#     turtle.pendown()
+#     turtle.color("pink")
+#     turtle.begin_fill()
+#     turtle.circle(25)
+#     turtle.end_fill()
+
+#     turtle.penup()
+#     turtle.goto(50,-200)
+#     turtle.pendown()
+#     turtle.color("pink")
+#     turtle.begin_fill()
+#     turtle.circle(25)
+#     turtle.end_fill()
+
+#     turtle.hideturtle()
+
+# draw_pig()
+
+# turtle.done()
+# -----------------------------------------------------------------
+# 【ACHV】: 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
